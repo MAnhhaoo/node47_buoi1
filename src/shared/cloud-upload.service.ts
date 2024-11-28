d3254ea7bf9a210cdffdbb1f1461e3245140bbcb
hao <macanhhao0347683890@gmail.com>
2024-11-28 19:30:07 +0700

    28/11

--- src/shared/cloud-upload.service.ts
+++ src/shared/cloud-upload.service.ts
@@ -11,13 +11,15 @@ export class CloudinaryUploadService{
     async uploadImage(file: Express.Multer.File, destination: string ): Promise<UploadApiResponse> {
         return new Promise((resolve , reject) => {
             const uploadStream = this.cloudinary.uploader.upload_stream(
-                {folder : destination}, // define folder tren cloudinary neu folder k co tren cloudinary thi se tao folder moi
+                {folder : destination}, 
+                // define folder tren cloudinary neu folder k co tren cloudinary thi se tao folder moi
                 (error :any , result: UploadApiResponse)=>{ // upload hinh len cloudinary
                     if(error) {
                         reject(error)
 
                     } else {
                         resolve(result)
+                        
                     }
                 }
             );
